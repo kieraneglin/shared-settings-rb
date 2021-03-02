6014e67880d4fdd092ed6ce3b1045464347b4e18
Kieran Eglin <kieran.eglin@gmail.com>
2021-03-02 14:11:42 -0800

    Make Rubocop happy

--- lib/shared-settings.rb
+++ lib/shared-settings.rb
@@ -1,4 +1,7 @@
+# rubocop:disable Naming/FileName
 # Due to autoloading conventions, the name of the entry file should be the same as the gem itself
 # As such, this is just a proxy for `shared_settings.rb`
 
 require 'shared_settings'
+
+# rubocop:enable Naming/FileName

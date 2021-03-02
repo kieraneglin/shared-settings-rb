a5f94a9c4053271c117e96c4d38761378b12ba2c
Kieran Eglin <kieran.eglin@gmail.com>
2021-03-02 14:08:42 -0800

    Added proxy file to allow Gem to be referenced as intended

---
+++ lib/shared-settings.rb
@@ -0,0 +1,4 @@
+# Due to autoloading conventions, the name of the entry file should be the same as the gem itself
+# As such, this is just a proxy for `shared_settings.rb`
+
+require 'shared_settings'
